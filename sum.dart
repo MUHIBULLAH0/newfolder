d2b2279c20620d1e864512e7aa5585f575aa9621
MUHIBULLAH0 <umuhib235@gmail.com>
2025-10-08 14:35:55 +0500

    take a input from user

--- sum.dart
+++ sum.dart
@@ -1,7 +1,12 @@
 // adding sumof two numbers
+import 'dart:io';
+
 void main(){
-int a=12;
-int b=60;
-print(a+b);
+  stdout.write("enter first number: ");
+int a=int.parse(stdin.readLineSync()!);
+  stdout.write("enter second number: ");
+int b=int.parse(stdin.readLineSync()!);
+int sum=a+b;
+print("the sum is $sum");
 
 }
