26918763cad57823e7f6b5fb470c7b7d3088885f
MUHIBULLAH0 <umuhib235@gmail.com>
2025-10-09 13:18:39 +0500

    if and else statment

---
+++ statement.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main(){
+  stdout.write('enter anumber');
+int num=int.parse(stdin.readLineSync()!);
+if(num%2==0){
+  print("the number is even");
+}
+else{
+  print("the number is odd");
+}
+
+}
