c62cbeb463487a38a4db43e24e69bf90640d0173
MUHIBULLAH0 <umuhib235@gmail.com>
2025-10-09 13:28:28 +0500

    class

---
+++ class.dart
@@ -0,0 +1,16 @@
+void main(){
+  person person1=person();
+  person1.display();
+  }
+class person{
+  String name='muhib';
+  int age=22;
+  void display(){
+    print("my name is $name \n and my age is $age");
+  }
+
+
+
+
+
+}
